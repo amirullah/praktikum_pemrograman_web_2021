9fce7fa67a760c756e2700750fdea8abb8cf05fc
amirullah <amir.pnl08@gmail.com>
2021-12-06 11:10:57 +0700

    Menamabah ubah dan hapus data barang

--- 2B_praktikum_1234567890123_amir/praktikum_11_1234567890123_amir/data_barang.php
+++ 2B_praktikum_1234567890123_amir/praktikum_11_1234567890123_amir/data_barang.php
@@ -61,15 +61,75 @@ $sql    = mysqli_query($conn, "SELECT * FROM tb_barang");
                                         <td><?php echo $data['keterangan']; ?></td>
                                         <td><?php echo $data['stok']; ?></td>
                                         <td>
-                                            <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <button data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $no ?>" type="button" class="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                                 </svg></button>
-                                            <button type="button" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#ModalHapus<?php echo $no ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                                                 </svg></button>
                                         </td>
                                     </tr>
+                                    <!-- Modal Edit Data Barang -->
+                                    <div class="modal fade" id="exampleModal<?php echo $no ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Ubah Data Barang</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <form method="POST" action="proses/proses_ubah_data_barang.php">
+                                                    <input type="hidden" name="kd_brg" value="<?php echo $data['kode_barang'] ?>">
+                                                    <div class="modal-body">
+                                                        <div class=" mb-3">
+                                                            <label class="col-form-label">Kode Barang:</label>
+                                                            <input type="number" class="form-control" value="<?php echo $data['kode_barang'] ?>" disabled>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <label class="col-form-label">Nama Barang:</label>
+                                                            <input name="nm_brg" type="text" class="form-control" value="<?php echo $data['nama_barang'] ?>">
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <label class="col-form-label">Keterangan:</label>
+                                                            <input name="ket" type="text" class="form-control" value="<?php echo $data['keterangan'] ?>">
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <label class="col-form-label">Stok Barang:</label>
+                                                            <input name="stok" type="text" class="form-control" value="<?php echo $data['stok'] ?>">
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Akhir Modal Edit Data Barang -->
+
+                                    <!-- Modal Hapus Data Barang -->
+                                    <div class="modal fade" id="ModalHapus<?php echo $no ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-md">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Hapus Data Barang</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <form method="POST" action="proses/proses_hapus_data_barang.php">
+                                                    <input type="hidden" name="kd_brg" value="<?php echo $data['kode_barang'] ?>">
+                                                    <div class="modal-body">
+                                                        Apakah anda yakin akan menghapus data "<?php echo $data['nama_barang']; ?>"?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                                        <button type="submit" class="btn btn-danger">Hapus</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Akhir Modal Hapus Data Barang -->
                                 <?php
                                 } ?>
                             </tbody>
@@ -85,26 +145,6 @@ $sql    = mysqli_query($conn, "SELECT * FROM tb_barang");
         <!-- Akhir footer -->
     </div>
     <!-- akhir sidebar -->
-
-    <!-- Modal Edit Data Barang -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Akhir Modal Edit Data Barang -->
 
     <!-- Optional JavaScript; choose one of the two! -->
 
