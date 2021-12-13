424e90319e48fc1bb46211e0fdf9c3fb5dcf48c5
amirullah <amir.pnl08@gmail.com>
2021-12-13 08:04:24 +0700

    menambah praktikum 12

---
+++ 2B_praktikum_1234567890123_amir/praktikum_12_1234567890123_amir/componen/header.php
@@ -0,0 +1,36 @@
+<?php
+require "proses/koneksi.php";
+$hasil = mysqli_query($conn, "select foto,level from tb_user WHERE username='$_SESSION[username]'");
+$row = mysqli_fetch_array($hasil);
+?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="index.php">SIPBAR</a>
+            <div class="dropdown ms-auto me-5 pe-3">
+                <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="<?php echo $row['foto']; ?>" alt="" width="32" height="32" class="rounded-circle">
+                    <strong><?php echo $_SESSION['username']; ?></strong>
+                </a>
+                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                    <li><a class="dropdown-item" href="#">New project...</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="profile">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="proses/logout.php">Sign out</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form> -->
+    </div>
+    </div>
+</nav>
---
+++ 2B_praktikum_1234567890123_amir/praktikum_12_1234567890123_amir/index.php
@@ -0,0 +1,18 @@
+<?php
+if (empty($_GET['x'])) {
+    echo "<script>window.location ='home'</script>";
+} elseif ($_GET['x'] == 'home') {
+    require "home.php";
+} elseif ($_GET['x'] == 'databarang') {
+    require "data_barang.php";
+} elseif ($_GET['x'] == 'datapeminjaman') {
+    require "data_peminjaman.php";
+} elseif ($_GET['x'] == 'mahasiswa') {
+    require "mahasiswa.php";
+} elseif ($_GET['x'] == 'profile') {
+    require "profile.php";
+} elseif ($_GET['x'] == 'peminjaman') {
+    require "peminjaman.php";
+} else {
+    echo "<script>window.location ='home'</script>";
+}
